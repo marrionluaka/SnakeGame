406183437eff9f99e3a9dfec52a8c2fa791df385
Marrion Lulu <marrion91300@gmail.com>
2021-12-15 15:27:50 -0800

    Adding animation speed logic

--- src/features/snake/modules/animationModule.ts
+++ src/features/snake/modules/animationModule.ts
@@ -3,27 +3,34 @@ import { curry } from 'ramda'
 interface Animation {
   start: () => void
   stop: () => void
+  increaseSpeed: (animationSpeed: number) => void
 }
 
 const createAnimation = (fn: Function): Animation => {
-  let animationRef: number
+  let _animationRef: number
+  let _animationDuration: number = 200
 
   const innerFn = curry((previousTimestamp: number, timestamp: number) => {
-    if (timestamp - previousTimestamp > 200) {
+    if (timestamp - previousTimestamp > _animationDuration) {
       fn()
-      animationRef = requestAnimationFrame(innerFn(timestamp))
+      _animationRef = requestAnimationFrame(innerFn(timestamp))
     } else {
-      animationRef = requestAnimationFrame(innerFn(previousTimestamp))
+      _animationRef = requestAnimationFrame(innerFn(previousTimestamp))
     }
   })
 
   return {
     start: () => {
-      animationRef = requestAnimationFrame(innerFn(0))
+      _animationRef = requestAnimationFrame(innerFn(0))
     },
 
     stop: () => {
-      cancelAnimationFrame(animationRef)
+      cancelAnimationFrame(_animationRef)
+      _animationDuration = 200
+    },
+
+    increaseSpeed: (animationSpeed: number) => {
+      _animationDuration = animationSpeed
     }
   }
 }
