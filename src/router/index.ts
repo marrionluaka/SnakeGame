5b152e068bba44969210fd3ad46f3c5df951fd39
Marrion Lulu <marrion91300@gmail.com>
2022-05-04 21:17:28 -0700

    Making stand alone snake game

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-import { SNAKE_GAME } from './routes'
-
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue')
-  },
-  {
-    path: '/snake',
-    name: SNAKE_GAME,
-    component: () => import('@/views/Snake.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,20 @@
 <template lang="pug">
-main.container.leading-normal.my-12.mx-auto.px-4.max-w-5xl
-  .mb-12
-    h1.text-5xl Browser Games
-
-  .grid.grid-cols-2.gap-4
-    div(v-for="route in routes")
-      a(v-if="route.link" :href="route.link" target="_blank")
-        img.block.h-auto.w-full.rounded-xl(:src="route.img_path" :alt="route.alt")
-      router-link(v-else :to="route.route_params")
-        img.block.h-auto.w-full.rounded-xl(:src="route.img_path" :alt="route.alt")
-      h2.text-2xl.pt-2 {{ route.title }}
+main.snake-game.flex.justify-center.items-center.text-white.h-full
+  SnakeCanvas
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import routes from '@/router/routes'
+import { SnakeCanvas } from '@/features/snake'
 
 export default defineComponent({
-  name: 'Home',
-
-  setup() {
-    return { routes }
-  }
+  name: 'Snake',
+  components: { SnakeCanvas }
 })
 </script>
+
+<style lang="stylus" scoped>
+.snake-game
+  background: #121212
+</style>
